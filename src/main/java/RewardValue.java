a82de8621128d232de73735e5a0029610e195e79
moonchiii <millywengg@gmail.com>
2023-08-02 15:35:05 -0400

    hello

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,20 @@
+public class RewardValue {
+    double cashValue;
+    double milesValue;
+
+    public RewardValue(double cashValue){
+        this.cashValue=cashValue;
+        this.milesValue=cashValue*0.0035;
+    }
+    public RewardValue(double cashValue, double milesValue){
+        this.cashValue=cashValue;
+        this.milesValue=milesValue;
+    }
+    public double getCashValue(){
+        return cashValue;
+    }
+
+    public double getMilesValue(){
+        return milesValue;
+    }
+}
